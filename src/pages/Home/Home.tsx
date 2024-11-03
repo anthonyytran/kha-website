5d5a414569407daca4dcadd1a82d8c86929ad052
DESKTOP-LGO4SDH\Anthony <anthony.tran99@hotmail.com>
2024-11-03 14:07:48 +1100

    .

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
+import KhaGroupPhoto1 from "../../assets/images/group-photo1.jpg";
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1>Home</h1>
+      <img src={KhaGroupPhoto1} alt="Group Photo 1" />
     </div>
   );
 };
