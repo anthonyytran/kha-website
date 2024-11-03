c6d684a5e6d06b4f8489da6dfa9716401326c2fc
DESKTOP-LGO4SDH\Anthony <anthony.tran99@hotmail.com>
2024-11-03 21:47:39 +1100

    latest

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -30,7 +30,48 @@ const Home = () => {
       <div className="home-container-2">
         <div className="record-text">
           <h2>Record</h2>
-          <p>7-0-0</p>
+          <p className="record-summary">7-0-0</p>
+          <h3 className="last-three-text">Last 3 Fights</h3>
+          <div className="fight-history">
+            <div className="fight-card">
+              <p>
+                <strong>Opponent:</strong> John Doe
+              </p>
+              <p>
+                <strong>Date:</strong> June 12, 2023
+              </p>
+              <p className="win">
+                <strong>Result:</strong> Win
+              </p>
+            </div>
+            <div className="fight-card">
+              <p>
+                <strong>Opponent:</strong> Mark Smith
+              </p>
+              <p>
+                <strong>Date:</strong> March 9, 2023
+              </p>
+              <p className="win">
+                <strong>Result:</strong> Win
+              </p>
+            </div>
+            <div className="fight-card">
+              <p>
+                <strong>Opponent:</strong> Alex Brown
+              </p>
+              <p>
+                <strong>Date:</strong> January 15, 2023
+              </p>
+              <p className="win">
+                <strong>Result:</strong> Win
+              </p>
+            </div>
+          </div>
+          <div className="view-all-button-container">
+            <Link to="/record" className="view-all-button">
+              View All
+            </Link>
+          </div>
         </div>
       </div>
     </div>
