d66a1369f4abe8a2fe8985c607e22c813158dac6
DESKTOP-LGO4SDH\Anthony <anthony.tran99@hotmail.com>
2025-03-15 23:53:21 +1100

    HomeSponsor refactor

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,14 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./Home.css";
 
 import UpcomingFight from "../../components/UpcomingFight/UpcomingFight";
 import HomeRecord from "../../components/HomeRecord/HomeRecord";
 import HomepageVideo from "../../components/HomepageVideo/HomepageVideo";
 import FightHistory from "../../components/FightHistory/FightHistory";
-
-// Sponsor images
-import sponsor1 from "../../assets/images/rephaze.png";
-import sponsor2 from "../../assets/images/studio25.png";
-import sponsor3 from "../../assets/images/theboxinglab.png";
-import sponsor4 from "../../assets/images/dynamic-therapy.png";
-import sponsor5 from "../../assets/images/greeneflicks.png";
+import HomeSponsors from "../../components/HomeSponsors/HomeSponsors";
 
 const Home: React.FC = () => {
   useEffect(() => {
@@ -27,39 +18,6 @@ const Home: React.FC = () => {
     });
     window.scrollTo(0, 0);
   }, []);
-
-  const sponsorSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
 
   return (
     <div>
@@ -96,33 +54,7 @@ const Home: React.FC = () => {
       <FightHistory />
 
       {/* Sponsors Section */}
-      <div className="home-container-3">
-        <h2 className="sponsors-title">Our Sponsors</h2>
-        <div className="sponsor-carousel">
-          <Slider {...sponsorSettings}>
-            <div className="sponsor-slide">
-              <img src={sponsor1} alt="Rephaze" />
-            </div>
-            <div className="sponsor-slide">
-              <img src={sponsor2} alt="Sponsor 2" />
-            </div>
-            <div className="sponsor-slide">
-              <img src={sponsor3} alt="The Boxing Lab" />
-            </div>
-            <div className="sponsor-slide">
-              <img src={sponsor4} alt="Dynamic Therapy" />
-            </div>
-            <div className="sponsor-slide">
-              <img src={sponsor5} alt="Greene Flicks" />
-            </div>
-          </Slider>
-        </div>
-        <div className="sponsor-button-container">
-          <Link to="/sponsors" className="sponsor-button">
-            View All
-          </Link>
-        </div>
-      </div>
+      <HomeSponsors />
     </div>
   );
 };
