7665f7960a61b9d81461690ae2d8054f8010fab1
DESKTOP-LGO4SDH\Anthony <anthony.tran99@hotmail.com>
2025-03-16 19:58:59 +1100

    adjustments

--- src/components/HomeSponsors/HomeSponsors.tsx
+++ src/components/HomeSponsors/HomeSponsors.tsx
@@ -15,9 +15,6 @@ import goldSponsor4 from "../../assets/images/bosker.jpg";
 import sponsor1 from "../../assets/images/rephaze.png";
 import sponsor2 from "../../assets/images/studio25.png";
 import sponsor3 from "../../assets/images/theboxinglab.png";
-import sponsor4 from "../../assets/images/dynamic-therapy.png";
-import sponsor5 from "../../assets/images/greeneflicks.png";
-import sponsor6 from "../../assets/images/a1.jpg";
 import sponsor7 from "../../assets/images/nimbus.png";
 
 const HomeSponsors: React.FC = () => {
@@ -29,10 +26,7 @@ const HomeSponsors: React.FC = () => {
     { id: 5, name: "Re:Phaze", image: sponsor1, tier: "silver" },
     { id: 6, name: "Studio 25", image: sponsor2, tier: "silver" },
     { id: 7, name: "The Boxing Lab", image: sponsor3, tier: "silver" },
-    { id: 8, name: "Dynamic Therapy", image: sponsor4, tier: "silver" },
-    { id: 9, name: "Greene Flicks", image: sponsor5, tier: "silver" },
-    { id: 10, name: "A1 Performance", image: sponsor6, tier: "silver" },
-    { id: 11, name: "Nimbus Studios", image: sponsor7, tier: "silver" },
+    { id: 8, name: "Nimbus Studios", image: sponsor7, tier: "silver" },
   ];
 
   const sponsorSettings = {
@@ -66,7 +60,7 @@ const HomeSponsors: React.FC = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          arrows: false,
+          arrows: true, // Changed from false to true to ensure arrows are visible on mobile
         },
       },
     ],
