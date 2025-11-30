76159890a70009fb245f313632f89ff8122206fa
unknown <anthony.tran99@hotmail.com>
2025-11-30 13:19:11 +1100

    Deleted from repo

--- src/components/UpcomingFight/UpcomingFight.tsx
+++
@@ -1,6 +0,0 @@
-// UpcomingFight component intentionally removed. This file now exports a harmless stub to avoid build errors
-import React from "react";
-
-const UpcomingFight: React.FC = () => null;
-
-export default UpcomingFight;
