f57394e715b1100ebf7b421716c661b06741e324
unknown <anthony.tran99@hotmail.com>
2026-08-11 21:36:00 +1000

    Home page refacotor

--- src/components/HomeVideo/HomeVideo.tsx
+++ src/components/HomeVideo/HomeVideo.tsx
@@ -3,15 +3,17 @@ import "./HomeVideo.css";
 
 const HomepageVideo: React.FC = () => {
   return (
-    <div className="homepage-video-container">
-      <video
-        className="homepage-video"
-        autoPlay
-        loop
-        muted
-        playsInline
-        src="/video/kha-vid.mp4"
-      />
+    <div className="home-video-section">
+      <div className="homepage-video-container">
+        <video
+          className="homepage-video"
+          autoPlay
+          loop
+          muted
+          playsInline
+          src="/video/kha-vid.mp4"
+        />
+      </div>
     </div>
   );
 };
